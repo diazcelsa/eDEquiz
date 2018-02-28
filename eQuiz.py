e83306ffc0b93db7a6c9c9de3e34ffcb3a8725e9
Celsa Diaz <celsadiaz@Celsas-MacBook-Air.local>
2018-02-28 16:12:22 +0100

    update edequiz to work for different types of tests

--- eQuiz.py
+++ eQuiz.py
@@ -6,165 +6,173 @@
 ######################################################################
 
 import numpy as np
-import sys
 import pandas as pd
-import datetime as dt
+import argparse
 
 
-class Namen:
-    def __init__(self, df):
-        df1 = df[df.columns[0:2]].copy()
-        df2 = df[df.columns[2:4]].copy()
-        df3 = df[df.columns[4:6]].copy()
-        df1.columns = ['a', 'b']
-        df2.columns = ['a', 'b']
-        df3.columns = ['a', 'b']
-        df1.loc[slice(None), 'location'] = 'eKuche'
-        df2.loc[slice(None), 'location'] = 'rBalkon'
-        df3.loc[slice(None), 'location'] = 'sZimmer'
-        df4 = pd.concat([df1, df2, df3], axis=0)
-        df4 = df4[pd.notnull(df4['b'])]
-        df4.loc[slice(None), 'match'] = 0
-        df = df4.reset_index()
-        df = df[df.columns[1:5]]
-        self.dafa = df
-        self.index = self.dafa.index
+def transform_and_suffle_namen_split_input(data, num_samples):
+    # transform splitted namen input data
+    die = data.loc[:, ['eKuche', 'Unnamed: 1']].rename(columns={'eKuche': 'name', 'Unnamed: 1': 'meaning'})
+    der = data.loc[:, ['rBalkon', 'Unnamed: 3']].rename(columns={'rBalkon': 'name', 'Unnamed: 3': 'meaning'})
+    das = data.loc[:, ['sZimmer', 'Unnamed: 5']].rename(columns={'sZimmer': 'name', 'Unnamed: 5': 'meaning'})
+    die.loc[:, 'gender'] = 'die'
+    der.loc[:, 'gender'] = 'der'
+    das.loc[:, 'gender'] = 'das'
+    df_restructured = pd.concat([die, der, das], axis=0).dropna()
 
-    def randomindex(self):
-        self.items = np.random.choice(range(len(self.index)), len(self.index), replace=False)
+    # select and suffle input data
+    selection = df_restructured.index.tolist()
+    np.random.shuffle(selection)
+    selection = selection[:num_samples]
+
+    return df_restructured.loc[selection, :]
 
 
-class Verben:
-    def __init__(self, df):
-        df1 = df[df.columns[0:2]].copy()
-        df2 = df[df.columns[2:4]].copy()
-        df1.columns = ['a', 'b']
-        df2.columns = ['a', 'b']
-        df1.loc[slice(None), 'objtype'] = 0  # 0 for akk
-        df2.loc[slice(None), 'objtype'] = 1  # 1 for dat
-        df3 = pd.concat([df1, df2], axis=0)
-        df3 = df3[pd.notnull(df3['b'])]
-        df3['match'] = 0
-        df = df3.reset_index()
-        df = df[df.columns[1:6]]
-        self.dafa = df
-        self.german = self.dafa.a
-        self.index = self.dafa.index
+def transform_and_suffle_namen_wo_split_input(data, num_samples):
+    # transform namen input data
+    data['gender'] = data['Namen'].str.split(' ').apply(lambda x: "".join(x[0]))
+    data['name'] = data['Namen'].str.split(' ').apply(lambda x: " ".join(x[1:]))
+    data['gender'] = data['gender'].replace('e', 'die').replace('r', 'der').replace('s', 'das')
+    df_restructured = data.drop(['Namen'], axis=1)
 
-    def randomindex(self):
-        self.items = np.random.choice(range(len(self.index)), len(self.index), replace=False)
+    # select and suffle input data
+    np.random.seed(0)
+    selection = df_restructured.index.tolist()
+    np.random.shuffle(selection)
+    selection = selection[:num_samples]
+
+    return df_restructured.loc[selection, :]
 
 
-class AdjectivAdverbAusdruck:
-    def __init__(self, df):
-        df.columns = ['a', 'b']
-        df['match'] = 0
-        self.dafa = df
-        self.index = self.dafa.index
+def transform_and_suffle_verben_akk_dat_input(data, num_samples):
+    # transform splitted namen input data
+    akk = data.loc[:, ['Akkusative', 'Unnamed: 1']].rename(columns={'Akkusative': 'verb', 'Unnamed: 1': 'meaning'})
+    dat = data.loc[:, ['Dativ', 'Unnamed: 3']].rename(columns={'Dativ': 'verb', 'Unnamed: 3': 'meaning'})
+    akk.loc[:, 'object'] = 'akk'
+    dat.loc[:, 'object'] = 'dat'
+    df_restructured = pd.concat([akk, dat], axis=0).dropna()
 
-    def randomindex(self):
-        self.items = np.random.choice(range(len(self.index)), len(self.index), replace=False)
+    # select and suffle input data
+    selection = df_restructured.index.tolist()
+    np.random.shuffle(selection)
+    selection = selection[:num_samples]
 
-
-class Results:
-    def __init__(self, df):
-        print(df)
-        self.dafa = df
-        self.num = len(self.dafa.index)
-        self.match = df['match']
-
-    def calculate(self):
-        self.right = self.match.tolist()
-        self.positive = self.right.count(1)
-        self.fraction = format(1.0 * self.positive / self.num, '.4f')
-        self.percentage = format((1.0 * self.positive * 100) / self.num, '.4f')
+    return df_restructured.loc[selection, :]
 
 
-#######################################################################################################
+def transform_and_suffle_starken_verben_input(data, num_samples):
+    # transform splitted namen input data
+    inf = data.loc[:, ['Infinitiv + Ergänzung', 'meaning']].rename(columns={'Infinitiv + Ergänzung': 'konjugation'})
+    pres = data.loc[:, ['3. Person Präsens', 'meaning']].rename(columns={'3. Person Präsens': 'konjugation'})
+    prae = data.loc[:, ['3. Person Präteritum', 'meaning']].rename(columns={'3. Person Präteritum': 'konjugation'})
+    part = data.loc[:, ['Hilfsverb + Partizip II', 'meaning']].rename(
+        columns={'Hilfsverb + Partizip II': 'konjugation'})
+    inf.loc[:, 'zeit'] = 'infinitiv'
+    pres.loc[:, 'zeit'] = '3_person_present'
+    prae.loc[:, 'zeit'] = '3_person_praeter'
+    part.loc[:, 'zeit'] = 'partII'
+    df_restructured = pd.concat([inf, pres, prae, part], axis=0).dropna()
+
+    # select and suffle input data
+    selection = df_restructured.index.tolist()
+    np.random.shuffle(selection)
+    selection = selection[:num_samples]
+
+    return df_restructured.loc[selection, :]
 
 
-# Get the input dataframe
-df = pd.read_csv(sys.argv[1], sep=',')
+def transform_and_suffle_wortschatz_input(data, num_samples):
+    # transform splitted namen input data
+    columns = data.columns.tolist()
+    df_restructured = data.rename(columns={columns[0]: 'wort', columns[1]: 'meaning'})
 
-# Clasify questions by type of data
-if len(df.columns) == 2:
-    obje = "Adjektiv adverbien und ausdruecke"
-    print(obje)
-    # Get words randomly order
-    mylist = AdjectivAdverbAusdruck(df)
-    mylist.randomindex()
-    finaldata = mylist.dafa
-    ran = mylist.items
-    for i in ran:
-        print("How would you say - %s - in german?" % finaldata.b[i])
-        mya = raw_input("Please enter the answer: ")
-        if mya == finaldata.a[i]:
-            print("Well done!")
-            finaldata.loc[i, 'match'] = 1
-        else:
-            print("Wrong! The answer was - %s -" % finaldata.a[i])
+    # select and suffle input data
+    selection = df_restructured.index.tolist()
+    np.random.shuffle(selection)
+    selection = selection[:num_samples]
+
+    return df_restructured.loc[selection, :]
 
 
-elif len(df.columns) == 4:
-    obje = "Verben"
-    print(obje)
-    # Get words randomly order
-    mylist = Verben(df)
-    mylist.randomindex()
-    ran = mylist.items
-    finaldata = mylist.dafa
-    for i in ran:
-        print("How would you say - %s - in german?" % finaldata.b[i])
-        mya = raw_input("Please enter the answer: ")
-        if mya == mylist.dafa.a[i]:
-            print("Well done! You know the translation but... is it akkusative?")
-            obj = raw_input("Please enter the answer (y/n): ")
-            if obj == 'y' and finaldata.objtype[i] == 0:
-                print("Well done! You are getting better ;)")
-                finaldata.loc[i, 'match'] = 1
-            elif obj == 'n' and finaldata.objtype[i] == 1:
-                print("Well done! You are getting better ;)")
-                finaldata.loc[i, 'match'] = 1
-            else:
-                print("Wrong!")
-        else:
-            print("Wrong! The answer was - %s -" % finaldata.a[i])
+def evaluate_user_answer_namen(row):
+    translation = input("How do you say {} in german? ".format(row['meaning']))
+    gender = input("What is the gender of {}? ".format(row['meaning']))
+    if translation == row['name'] and gender == row['gender']:
+        print("You were correct!")
+        answer = True
+    else:
+        print("You are wrong, the translation is {} and the gender is {}.".format(row['name'], row['gender']))
+        answer = False
+    return answer
 
 
-elif len(df.columns) == 6:
-    obje = "Namen"
-    print(obje)
-    # Get words randomly order
-    mylist = Namen(df)
-    mylist.randomindex()
-    ran = mylist.items
-    finaldata = mylist.dafa
-    print(ran)
-    for i in ran:
-        print("How would you say - %s - in german?" % finaldata.b[i])
-        mya = raw_input("Please enter the answer: ")
-        if mya == finaldata.a[i]:
-            print("Well done! You know the translation but what is its gender or location?")
-            loc = raw_input("Please enter the answer (eKuche/rBalkon/sZimmer): ")
-            if loc == finaldata.location[i]:
-                print("Well done! You know the translation and the gender!")
-                finaldata.loc[i, 'match'] = 1
-            else:
-                print("Wrong! Sorry, it was:", finaldata.location[i],
-                      "You have to learn also the gender of the Namen...")
-        else:
-            print("Wrong! The answer was:", finaldata.a[i], "in", finaldata.location[i])
+def evaluate_user_answer_verben_akk_dat(row):
+    translation = input("How do you say {} in german? ".format(row['meaning']))
+    object_type = input("Which is the type of object with the verb {}? akk or dat? ".format(row['meaning']))
+    if translation == row['verb'] and object_type == row['object']:
+        print("You were correct!")
+        answer = True
+    else:
+        print("You are wrong, the translation is {} and the object type is {}.".format(row['verb'], row['object']))
+        answer = False
+    return answer
 
-# Get results
-myres = Results(finaldata)
-myres.calculate()
-words = str(myres.num)
-fract = str(myres.fraction)
-perce = str(myres.percentage)
-date = str(dt.datetime.now())
 
-# Save the results of each quiz in results.csv
-fd = open('results.csv', 'a')
-newrow = date + "," + obje + "," + words + "," + fract + "," + perce + "\n"
-fd.write(newrow)
-fd.close()
+def evaluate_user_answer_starken_verben(row):
+    translation = input("How is the {} of the verb \"{}\" in german? ".format(row['zeit'], row['meaning']))
+    if translation == row['konjugation']:
+        print("You were correct!")
+        answer = True
+    else:
+        print("You are wrong, the translation is {} and the time is {}.".format(row['zeit'], row['konjugation']))
+        answer = False
+    return answer
+
+
+def evaluate_user_answer_wortschatz(row):
+    translation = input("How do you say \"{}\" in german? ".format(row['meaning']))
+    if translation == row['wort']:
+        print("You were correct!")
+        answer = True
+    else:
+        print("You are wrong, the translation is {}.".format(row['wort']))
+        answer = False
+    return answer
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Test your german knowledge")
+    parser.add_argument('--task', choices=['namen_split', 'namen_wo_split', 'verben_akk_dat', 'verben_deklination',
+                                           'wortschatz'])
+    parser.add_argument('--input', help='./data/wortschatz.csv')
+    parser.add_argument('--num', help='Number of elements in the quiz to test.')
+
+    args = parser.parse_args()
+
+    if args.task == 'namen_split':
+        df_selection = transform_and_suffle_namen_split_input(args.input, args.num)
+        results = df_selection.apply(lambda x: evaluate_user_answer_namen(x), axis=1)
+
+    if args.task == 'namen_wo_split':
+        df_selection = transform_and_suffle_namen_wo_split_input(args.input, args.num)
+        results = df_selection.apply(lambda x: evaluate_user_answer_namen(x), axis=1)
+
+    if args.task == 'verben_akk_dat':
+        df_selection = transform_and_suffle_verben_akk_dat_input(args.input, args.num)
+        results = df_selection.apply(lambda x: evaluate_user_answer_verben_akk_dat(x), axis=1)
+
+    if args.task == 'verben_deklination':
+        df_selection = transform_and_suffle_starken_verben_input(args.input, args.num)
+        results = df_selection.apply(lambda x: evaluate_user_answer_starken_verben(x), axis=1)
+
+    if args.task == 'wortschatz':
+        df_selection = transform_and_suffle_wortschatz_input(args.input, args.num)
+        results = df_selection.apply(lambda x: evaluate_user_answer_wortschatz(x), axis=1)
+
+    print(
+        "You answer {} times correctly.\nThe percentage of correct answers is {}.".format(sum(results),
+                                                                                          sum(results) * 100 / args.num))
+
+
+if __name__ == "__main__":
+    main()
