1b3f6fa7eb69c9f31d2f4bd4176810b46d14b2b3
Celsa Diaz <celsadiaz@celsas-air.speedport.ip>
2018-04-19 18:03:48 +0200

    correct argparser

--- eQuiz.py
+++ eQuiz.py
@@ -20,12 +20,12 @@ class SmartFormatter(argparse.HelpFormatter):
 
 def transform_and_suffle_namen_split_input(data, num_samples):
     # transform splitted namen input data
-    die = data.loc[:, ['eKuche', 'Unnamed: 1']].rename(columns={'eKuche': 'name', 'Unnamed: 1': 'meaning'})
-    der = data.loc[:, ['rBalkon', 'Unnamed: 3']].rename(columns={'rBalkon': 'name', 'Unnamed: 3': 'meaning'})
-    das = data.loc[:, ['sZimmer', 'Unnamed: 5']].rename(columns={'sZimmer': 'name', 'Unnamed: 5': 'meaning'})
-    die.loc[:, 'gender'] = 'die'
-    der.loc[:, 'gender'] = 'der'
-    das.loc[:, 'gender'] = 'das'
+    die = data.ix[:, ['eKuche', 'Unnamed: 1']].rename(columns={'eKuche': 'name', 'Unnamed: 1': 'meaning'})
+    der = data.ix[:, ['rBalkon', 'Unnamed: 3']].rename(columns={'rBalkon': 'name', 'Unnamed: 3': 'meaning'})
+    das = data.ix[:, ['sZimmer', 'Unnamed: 5']].rename(columns={'sZimmer': 'name', 'Unnamed: 5': 'meaning'})
+    die['gender'] = 'die'
+    der['gender'] = 'der'
+    das['gender'] = 'das'
     df_restructured = pd.concat([die, der, das], axis=0).dropna()
 
     # select and suffle input data
@@ -54,10 +54,10 @@ def transform_and_suffle_namen_wo_split_input(data, num_samples):
 
 def transform_and_suffle_verben_akk_dat_input(data, num_samples):
     # transform splitted namen input data
-    akk = data.loc[:, ['Akkusative', 'Unnamed: 1']].rename(columns={'Akkusative': 'verb', 'Unnamed: 1': 'meaning'})
-    dat = data.loc[:, ['Dativ', 'Unnamed: 3']].rename(columns={'Dativ': 'verb', 'Unnamed: 3': 'meaning'})
-    akk.loc[:, 'object'] = 'akk'
-    dat.loc[:, 'object'] = 'dat'
+    akk = data.ix[:, ['Akkusative', 'Unnamed: 1']].rename(columns={'Akkusative': 'verb', 'Unnamed: 1': 'meaning'})
+    dat = data.ix[:, ['Dativ', 'Unnamed: 3']].rename(columns={'Dativ': 'verb', 'Unnamed: 3': 'meaning'})
+    akk['object'] = 'akk'
+    dat['object'] = 'dat'
     df_restructured = pd.concat([akk, dat], axis=0).dropna()
 
     # select and suffle input data
@@ -70,15 +70,15 @@ def transform_and_suffle_verben_akk_dat_input(data, num_samples):
 
 def transform_and_suffle_starken_verben_input(data, num_samples):
     # transform splitted namen input data
-    inf = data.loc[:, ['Infinitiv + Ergänzung', 'meaning']].rename(columns={'Infinitiv + Ergänzung': 'konjugation'})
-    pres = data.loc[:, ['3. Person Präsens', 'meaning']].rename(columns={'3. Person Präsens': 'konjugation'})
-    prae = data.loc[:, ['3. Person Präteritum', 'meaning']].rename(columns={'3. Person Präteritum': 'konjugation'})
-    part = data.loc[:, ['Hilfsverb + Partizip II', 'meaning']].rename(
+    inf = data.ix[:, ['Infinitiv + Ergänzung', 'meaning']].rename(columns={'Infinitiv + Ergänzung': 'konjugation'})
+    pres = data.ix[:, ['3. Person Präsens', 'meaning']].rename(columns={'3. Person Präsens': 'konjugation'})
+    prae = data.ix[:, ['3. Person Präteritum', 'meaning']].rename(columns={'3. Person Präteritum': 'konjugation'})
+    part = data.ix[:, ['Hilfsverb + Partizip II', 'meaning']].rename(
         columns={'Hilfsverb + Partizip II': 'konjugation'})
-    inf.loc[:, 'zeit'] = 'infinitiv'
-    pres.loc[:, 'zeit'] = '3_person_present'
-    prae.loc[:, 'zeit'] = '3_person_praeter'
-    part.loc[:, 'zeit'] = 'partII'
+    inf['zeit'] = 'infinitiv'
+    pres['zeit'] = '3_person_present'
+    prae['zeit'] = '3_person_praeter'
+    part['zeit'] = 'partII'
     df_restructured = pd.concat([inf, pres, prae, part], axis=0).dropna()
 
     # select and suffle input data
@@ -149,33 +149,34 @@ def evaluate_user_answer_wortschatz(row):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Test your german knowledge.", usage="\nUsage for quiz of names splitted by gender:\npython eQuiz.py -t namen_split -i path_to_your_csv.csv -n num_exercises\n\nUsage for quiz of names without splitting by gender:\npython eQuiz.py -t namen_wo_split -i path_to_your_csv.csv -n num_exercises\n\nUsage for quiz of verbs splitting by akusative or dative:\npython eQuiz.py -t verben_akk_dat -i path_to_your_csv.csv -n num_exercises\n\nUsage for quiz of verbs declination:\npython eQuiz.py -t verben_deklination -i path_to_your_csv.csv -n num_exercises\n\nUsage for quiz of simply vocabulary:\npython eQuiz.py -t wortschatz -i path_to_your_csv.csv -n num_exercises.", formatter_class=SmartFormatter)
+    parser = argparse.ArgumentParser(description="Test your german knowledge.", usage="\nUsage for quiz of names splitted by gender:\npython eQuiz.py --task namen_split --input path_to_your_csv.csv --num num_exercises\n\nUsage for quiz of names without splitting by gender:\npython eQuiz.py --task namen_wo_split --input path_to_your_csv.csv --num num_exercises\n\nUsage for quiz of verbs splitting by akusative or dative:\npython eQuiz.py --task verben_akk_dat --input path_to_your_csv.csv --num num_exercises\n\nUsage for quiz of verbs declination:\npython eQuiz.py --task verben_deklination --input path_to_your_csv.csv --num num_exercises\n\nUsage for quiz of simply vocabulary:\npython eQuiz.py --task wortschatz --input path_to_your_csv.csv --num num_exercises.", formatter_class=SmartFormatter)
 
-    parser.add_argument('--task', choices=['namen_split', 'namen_wo_split', 'verben_akk_dat', 'verben_deklination',
-                                           'wortschatz'])
+    parser.add_argument('--task', choices=['namen_split', 'namen_wo_split', 'verben_akk_dat', 'verben_deklination','wortschatz'])
     parser.add_argument('--input', help='./data/wortschatz.csv')
     parser.add_argument('--num', help='Number of elements in the quiz to test.')
 
     args = parser.parse_args()
+    input_data = pd.read_csv(args.input, sep="\t")
+    n_tests = int(args.num)
 
     if args.task == 'namen_split':
-        df_selection = transform_and_suffle_namen_split_input(args.input, args.num)
+        df_selection = transform_and_suffle_namen_split_input(input_data, n_tests)
         results = df_selection.apply(lambda x: evaluate_user_answer_namen(x), axis=1)
 
     if args.task == 'namen_wo_split':
-        df_selection = transform_and_suffle_namen_wo_split_input(args.input, args.num)
+        df_selection = transform_and_suffle_namen_wo_split_input(input_data, n_tests)
         results = df_selection.apply(lambda x: evaluate_user_answer_namen(x), axis=1)
 
     if args.task == 'verben_akk_dat':
-        df_selection = transform_and_suffle_verben_akk_dat_input(args.input, args.num)
+        df_selection = transform_and_suffle_verben_akk_dat_input(input_data, n_tests)
         results = df_selection.apply(lambda x: evaluate_user_answer_verben_akk_dat(x), axis=1)
 
     if args.task == 'verben_deklination':
-        df_selection = transform_and_suffle_starken_verben_input(args.input, args.num)
+        df_selection = transform_and_suffle_starken_verben_input(input_data, n_tests)
         results = df_selection.apply(lambda x: evaluate_user_answer_starken_verben(x), axis=1)
 
     if args.task == 'wortschatz':
-        df_selection = transform_and_suffle_wortschatz_input(args.input, args.num)
+        df_selection = transform_and_suffle_wortschatz_input(input_data, n_tests)
         results = df_selection.apply(lambda x: evaluate_user_answer_wortschatz(x), axis=1)
 
     print(
